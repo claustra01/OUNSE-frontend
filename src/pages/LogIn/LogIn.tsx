95eadaf7243547ad146773ad652955919df27178
Sea10wood <miyusea5@gmail.com>
2023-03-13 13:55:46 +0900

    LoginとSignUpPage（仮）

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -1,7 +1,58 @@
-export function LogIn() {
+import {
+    Box,
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    TextField,
+  } from "@mui/material";
+  import { memo } from "react";
+  
+  export const LogIn = memo(() => {
+    const cardStyle = {
+      display: "block",
+      transitionDuration: "0.3s",
+      height: "450px",
+      width: "400px",
+      variant: "outlined",
+    };
+  
     return (
-        <>
-            LogIn
-        </>
-    )
-}
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        padding={20}
+      >
+        <Card style={cardStyle}>
+          <CardHeader title="ログインページ" />
+          <CardContent>
+            <div>
+              <TextField
+                fullWidth
+                id="username"
+                type="email"
+                label="Username"
+                placeholder="Username"
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                id="password"
+                type="password"
+                label="Password"
+                placeholder="Password"
+                margin="normal"
+              />
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button variant="contained" size="large" color="secondary">
+              Login
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    );
+  });
