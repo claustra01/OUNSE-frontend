a249e37a69bbb6337f78a5a2e6aebbeee23196fa
sny430 <shinya430.g@gmail.com>
2023-03-13 15:47:27 +0900

    :technologist: add connect check button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 import './App.css';
@@ -10,35 +10,38 @@ import { SignUp } from './pages/SignUp/SignUp'
 
 function App() {
 
-    const serverURL: string = process.env.REACT_APP_SERVER_URL as string
-    const [connectionStatus, setConnectionStatus] = useState("")
+    const [connectionStatus, setConnectionStatus] = useState('')
 
     const ConnectionTest = () => {
         async function ConnectionTest() {
-            const res = await axios.get(serverURL)
-            console.log(serverURL)
+            const res = await axios.get('initialization')
+            console.log('initialization')
             console.log(res)
             setConnectionStatus(res.data)
         }
         ConnectionTest()
+    }
+
+    const CheckButton = () => {
+        return (
+            <div className="App">
+                <button onClick={ConnectionTest}>Button</button>
+                <br/>
+                {connectionStatus}
+            </div>
+        )
     }
       
     return (
         
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Top/>} />
+                <Route path="/" element={<><CheckButton/><Top/></>} />
                 <Route path="/home" element={<Home/>} />
                 <Route path="/login" element={<LogIn/>} />
                 <Route path="/signup" element={<SignUp/>} />
             </Routes>
         </BrowserRouter>
-
-        // <div className="App">
-        //     <button onClick={ConnectionTest}>Button</button>
-        //     <br/>
-        //     {connectionStatus}
-        // </div>
 
     );
 }
