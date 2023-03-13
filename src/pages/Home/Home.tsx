284e5ae738047e3a86e07109b3865abcdb7f1617
Sea10wood <miyusea5@gmail.com>
2023-03-14 08:35:41 +0900

    Toppageロゴ

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
 import { Box, Button, Card, CardActions, CardContent, CardHeader, TextField } from "@mui/material";
-import { Stack } from '@mui/material';
-import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-// import { useNavigate, Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useCookies } from "react-cookie";
 
-// const settings = [
-//     { label: 'ログアウト', link: '/'}
-//   ];
 
 function Home() {
         const [cookie, setCookie, removeCookie] = useCookies(["token"]);
@@ -26,11 +14,10 @@ function Home() {
                 removeCookie("token");
                 navigate('/')
               }
-            
-
 
     return (
         <>
+
             <div className="background-color"/>
             <AppBar position="static" style={{ backgroundColor: "#CDDFD3" }}>
                 <Button 
