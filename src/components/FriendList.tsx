e19a2a5f2ea0b250759f7f17d0449b127279db27
sny430 <shinya430.g@gmail.com>
2023-03-15 08:22:31 +0900

    :seedling: create friend data component

--- src/components/FriendList.tsx
+++ src/components/FriendList.tsx
@@ -1,6 +1,13 @@
+import FriendData from "./FriendData"
+
 function FriendList(props: {friends: string[]}) {
     return (
-        <></>
+        <>
+            {props.friends.map((v) => (
+                <FriendData data={v} />
+            ))}
+
+        </>
     )
 }
 
