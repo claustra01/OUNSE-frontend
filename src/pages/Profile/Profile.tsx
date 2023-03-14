97378900449c132a3ca1a634f0769be697f9a1e9
sny430 <shinya430.g@gmail.com>
2023-03-15 03:28:52 +0900

    :sparkles: send friend request

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,7 +1,31 @@
+import { Button, TextField } from '@mui/material';
+import { useState, useContext } from 'react';
+import { UserContext } from '../../App';
+import axios from 'axios';
+
 function Profile() {
+
+    const {userId} = useContext(UserContext)
+
+    // フレンドリクエスト
+    const [friendId, setFriendId] = useState('')
+
+    const reqFriend = async () => {
+        const res = await axios.put('sendfriend', {
+            user_id: userId,
+            friend_id: friendId
+        })
+        console.log(res.data)
+    }
+    
+
     return (
-        <></>
+        <>
+            <TextField onChange={(e) => setFriendId(e.target.value)} />
+            <Button onClick={reqFriend} >リクエスト</Button>
+        </>
     )
+    
 }
 
 export default Profile
