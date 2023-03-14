eafb810e2d87f6998c483ed4b78f34b49dcc9338
sny430 <shinya430.g@gmail.com>
2023-03-15 08:32:58 +0900

    :lipstick: update friend request ui

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from '@mui/material';
+import { Box, Button, TextField } from '@mui/material';
 import { useState, useContext, useEffect } from 'react';
 import { UserContext } from '../../App';
 import axios from 'axios';
@@ -22,6 +22,7 @@ function Profile() {
     
     // フレンド取得
     const [friendList, setFriendList] = useState([])
+    const [requestList, setRequestList] = useState([])
 
     useEffect(() => { 
         const getFriend = async () => {
@@ -29,6 +30,7 @@ function Profile() {
             const obj = JSON.parse(JSON.stringify(res));
             console.log(obj.data)
             setFriendList(obj.data.FriendList)
+            setRequestList(obj.data.RequestList)
         }
         if (userId !== '') getFriend()
     }, [userId])
@@ -36,8 +38,27 @@ function Profile() {
     return (
         <>
             <Header/>
-            <TextField onChange={(e) => setFriendId(e.target.value)} />
-            <Button onClick={reqFriend}>リクエスト</Button>
+            <Box
+                sx={{
+                    width: '30rem',
+                    opacity: 0.9,
+                }}
+                style={{ backgroundColor: "#FFFCEF" }}
+                >
+                <TextField
+                    id="friendReq" 
+                    label="Friend Request" 
+                    variant="standard"
+                    sx={{ m: "2em 3em 0 3em", height: "5em", width: "20em" }}
+                    onChange={(e) => setFriendId(e.target.value)}
+                />
+                <Button 
+                    variant="contained"
+                    sx={{ m: "0em 0em 0em 10em ", height: "3em", width: "10em", textAlign:"center", justifyContent: "center", alignItems: "center", display:"flex" }} 
+                    style={{ backgroundColor: "#388e3c" }}
+                    onClick={reqFriend}
+                >リクエスト送信</Button>
+            </Box>
             <FriendList friends={friendList} />
         </>
     )
