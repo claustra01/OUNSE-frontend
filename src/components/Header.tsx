c36e25bf4522619037fc7f2dbebac388307be84b
sny430 <shinya430.g@gmail.com>
2023-03-15 12:32:48 +0900

    :lipstick: fix logout button pos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,45 @@
-
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import TeamName from "./mastdonteamlogo.png";
+import { Button } from '@mui/material';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+
+    // ログアウト
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [cookie, setCookie, removeCookie] = useCookies(["token"]);
+    const navigate = useNavigate()
+
+    const clickLogOut = () => {
+        removeCookie("token");
+        navigate('/')
+    }
   
   return (
-    
 
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static"
-       style={{backgroundColor: "#0DA242", height:"40px"}}>
+       style={{backgroundColor: "#0DA242", height:"50px"}}>
       
-      <img src={TeamName} alt=""  width="120px"  text-align= "left">
+      <img src={TeamName} alt="" width="120px"  text-align= "left">
       </img>
   
       </AppBar>
+
+      <Button 
+        variant="contained" 
+        sx={{
+          m: "-6em 2em 0 90%",
+          width:"7%",
+          justifyContent:"center",
+          textAlign: "center",
+        }}
+        onClick={clickLogOut}
+        style={{ backgroundColor: "#388e3c" }}
+      >Logout</Button> 
+        
     </Box>
   );
 }
